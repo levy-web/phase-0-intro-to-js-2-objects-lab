80985b71a130640fa0541f15798bdf72343eef2c
levy <lnjiru43@gmail.com>
2022-11-19 01:09:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name:"levy",
+    streetAddress:"0000",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;    
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const clone = {...employee}
+    delete clone[key];
+    return clone;
+}function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
